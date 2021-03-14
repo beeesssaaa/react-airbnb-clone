b244297d7824c88ac8d9dd8eb9b21bcc63a7a748
beeesssaaa <13708919+beeesssaaa@users.noreply.github.com>
2021-03-14 14:44:07 +0900

    Added banner and search

--- src/Header.js
+++ src/Header.js
@@ -8,12 +8,11 @@ import { Avatar } from '@material-ui/core';
 function Header() {
     return (
         <div className="header">
-            <div className="header__icon">
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/220px-Airbnb_Logo_B%C3%A9lo.svg.png" 
-                    alt="airbnb"
-                />
-            </div>
+            <img
+                className="header__icon"
+                src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" 
+                alt="airbnb"
+            />
             
             <div className="header__center">
                 <input type="text"/>
